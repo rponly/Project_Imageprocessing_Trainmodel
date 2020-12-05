e290b615e35dcb2752206e7d077d97a97b6509d1
rponly <43706421+rponly@users.noreply.github.com>
2020-12-05 18:12:55 +0700

    Add files via upload

--- SVM+MLP.py
+++ SVM+MLP.py
@@ -18,7 +18,9 @@ import functools
 from os.path import basename
 from imutils import paths
 import pickle
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.ensemble import VotingClassifier
+from sklearn.utils.estimator_checks import check_estimator
 def image_to_feature_vector(image, size=(32, 32)):
 
 	return cv2.resize(image, size).flatten()
@@ -27,12 +29,14 @@ def image_to_feature_vector(image, size=(32, 32)):
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
 	help="path to input dataset")
+ap.add_argument("-p", "--Predictfeature", required=True,
+	help="path to Predict feature")
 args = vars(ap.parse_args())
 imagePaths = list(paths.list_images(args["dataset"]))
-
+testPaths = list(paths.list_images(args["Predictfeature"]))
 features = []
 labels = []
-
+testfeatures = []
 for (i, imagePath) in enumerate(imagePaths):
 	# load the image and extract the class label (assuming that our
 	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
@@ -48,16 +52,28 @@ for (i, imagePath) in enumerate(imagePaths):
 	if i > 0 and i % 1000 == 0:
 		print("[INFO] processed {}/{}".format(i, len(imagePaths)))
 
+for (i, testPaths) in enumerate(testPaths):
+	# load the image and extract the class label (assuming that our
+	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+	image = cv2.imread(testPaths)
+	
+
+	pixels = image_to_feature_vector(image)
+	testfeatures.append(pixels)
+	
 # show some information on the memory consumed by the raw images
 # matrix and features matrix
 features = np.array(features)
 labels = np.array(labels)
+#testfeatures = np.array(testfeatures)
+estimators = []
 
 #SVM
 from sklearn.svm import SVC
 svclassifier = SVC(kernel='linear')
 params_svm = {"kernel":"rbf", "C":10, "gamma":0.000001}
 svclassifier.set_params(**params_svm)
+estimators.append(('svm', svclassifier))
 svclassifier.fit(features, labels)
 filename = 'SVM_finalized_model.sav'
 pickle.dump(svclassifier, open(filename, 'wb'))
@@ -70,9 +86,30 @@ print(score)
 #MLP
 from sklearn.neural_network import MLPClassifier
 mlp = MLPClassifier(hidden_layer_sizes=(88,), activation='relu', solver='lbfgs', max_iter=500 ,random_state=42) #48 72 80 r 70
+estimators.append(('mlp', mlp))
 mlp.fit(features,labels)
 filename = 'MLP_finalized_model.sav'
 pickle.dump(mlp, open(filename, 'wb'))
 mlp_predict_result = mlp.predict(testfeatures)
 print ("mlp predict")
 print(mlp_predict_result)
+
+#KNN
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5)
+estimators.append(('knn', knn))
+knn.fit(features, labels)
+filename = 'KNN_finalized_model.sav'
+pickle.dump(knn, open(filename, 'wb'))
+knn_predict_result = knn.predict(testfeatures)
+print ("knn predict")
+print(knn_predict_result)
+
+
+ensemble = VotingClassifier(estimators)
+ensemble.fit(features, labels)
+result = ensemble.predict(testfeatures)
+print("Voted")
+print(result)
+""" score_1 = ensemble.score(X_test, y_test)
+print(score_1) """
--- Test.py
+++ Test.py
@@ -1,21 +1,45 @@
+import cv2
+import numpy as np
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 import pickle
-#path
-testfeatures = []
+import argparse
+from os.path import basename
+from imutils import paths
+def image_to_feature_vector(image, size=(32, 32)):
 
-Featureimage =cv2.imread(path)
-testimage = image_to_feature_vector(Featureimage)
-testfeatures.append(testimage)
+	return cv2.resize(image, size).flatten()
+#path
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--Predictfeature", required=True,
+	help="path to Predict feature")
+args = vars(ap.parse_args())
+testPaths = list(paths.list_images(args["Predictfeature"]))
+testfeatures = []
+for (i, testPaths) in enumerate(testPaths):
+	# load the image and extract the class label (assuming that our
+	# path as the format: /path/to/dataset/{class}.{image_num}.jpg
+	image = cv2.imread(testPaths)
+	
+
+	pixels = image_to_feature_vector(image)
+	testfeatures.append(pixels)
 testfeatures = np.array(testfeatures)
+
 
 filename = 'SVM_finalized_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 SVM_result = loaded_model.predict(testfeatures)
-print(result)
+print(SVM_result)
 
 filename = 'MLP_finalized_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 MLP_result = loaded_model.predict(testfeatures)
 print ("mlp predict")
 print(MLP_result)
+
+filename = 'KNN_finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+KNN_result = loaded_model.predict(testfeatures)
+print ("knn predict")
+print(KNN_result)
